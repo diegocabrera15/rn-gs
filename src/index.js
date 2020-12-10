1d74df68018c47089db11f96fd669579a4697635
Diego Cabrera <diego.cabrera.15@outlook.com>
2020-12-09 23:14:12 -0500

    Explanation export default and only export

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// import { heroes } from "./data/heroes";
+
+import heroes from "./data/heroes";
+
+const getHeoreByID = (id) => {
+  return heroes.find((el) => el.id === id);
+};
+
+// console.log(getHeoreByID(2));
+
+const getHeroesByOwner = (owner) => {
+  return heroes.filter((el) => el.owner === owner);
+};
+
+console.log(getHeroesByOwner('Marvel'));
